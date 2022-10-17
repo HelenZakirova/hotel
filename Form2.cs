e3df258ab945163bb320490f167d4f334edd23fc
HelenZakirova <115071990+HelenZakirova@users.noreply.github.com>
2022-10-17 12:52:50 +0700

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hotel
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            livers Win = new livers();
+            Win.Owner = this;
+            this.Hide();
+            Win.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            workers Win = new workers();
+            Win.Owner = this;
+            this.Hide();
+            Win.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            positions Win = new positions();
+            Win.Owner = this;
+            this.Hide();
+            Win.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            rooms Win = new rooms();
+            Win.Owner = this;
+            this.Hide();
+            Win.Show();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            reg Win = new reg();
+            Win.Owner = this;
+            this.Hide();
+            Win.Show();
+        }
+    }
+}
